35dbbd238be85c18ce81853775480b97513b3161
wiresavvy <maisonatelier52@gmail.com>
2026-01-22 16:45:09 +0530

    [Fix] header

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       {/* Header */}
       <header className="absolute top-0 left-0 z-20 w-full">
-        <div className="mx-auto flex max-w-7xl items-center px-16 py-6">
+        <div className="flex items-center py-6 px-15">
           {/* Portfolio Box */}
           <a
             href="#"
